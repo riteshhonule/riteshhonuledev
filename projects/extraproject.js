103cf55edadb8174e42c11d718bb50edf89c0d85
Tech_Mr. <140720184+riteshhonule@users.noreply.github.com>
2025-10-26 23:28:18 +0530

    Add files via upload

---
+++ projects/extraproject.js
@@ -0,0 +1,10 @@
+// {
+//     "name": "Moving Object Detection",
+//     "desc": "A Python project using OpenCV to detect and highlight moving objects in real-time from a webcam feed.",
+//     "image": "moving",
+//     "category": "Artificial Intelligence",
+//     "links": {
+//       "view": "https://www.linkedin.com/in/ritesh-honule-1b8976285/?originalSubdomain=in",
+//       "code": "https://github.com/riteshhonule/Moving_object_detection"
+//     }
+//   },
